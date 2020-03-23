6726a6ed78a642537a7ab9c04452fcf153037d0c
Emmanuel Leguizamon <62218305+Emalegui95@users.noreply.github.com>
2020-03-23 20:09:43 -0300

    Ejercicio VII

---
+++ CAI_EjercicioVI/CAI_EjercicioVI/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAI_EjercicioVI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] numeros = new int[5];
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Ingrese un número: ");
+                numeros[i] = int.Parse(Console.ReadLine());
+            }
+
+            Array.Sort(numeros);
+
+            Console.WriteLine("El número más bajo es: " + numeros [0]);
+
+            Console.WriteLine("El número más alto es: " + numeros[4]);
+
+            Console.WriteLine("El número del medio es: " + numeros[2]);
+
+            Console.ReadKey(true);
+        }
+        
+    }
+}
